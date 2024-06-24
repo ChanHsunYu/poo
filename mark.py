1c77dd1abffdffd4b4b48798d10439da9a0a14e2
ChanHsunYu <mars20120718@gmail.com>
2024-06-24 16:18:32 +0800

    Add files via upload

---
+++ mark.py
@@ -0,0 +1,10 @@
+X = 3
+math = input('3x-12+24=24 x=多少?:')
+if math == '4':
+	print('你答對了, Congratulations!')
+if math != '4':
+	print('答錯了, Wawawa.')
+if math > '4':
+	print('太多囉!, It is more!')
+if math < '4':
+	print('太少囉!, It is less!')
